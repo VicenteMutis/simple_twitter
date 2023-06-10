002f1114fa422896c960585750966e924f9ba9e7
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:48:12 -0400

    added the index controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,6 @@
+class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all #si es un @, es una variable global. Vive más que śolo en este método, se puede acceder desde cualquier parte de la clase.
+    end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static_pages#home"
+
+  resources :tweets, except: [:update, :edit]
+
 end
