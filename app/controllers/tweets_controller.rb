98ba446ea7ed40a4dffdbd5fab8d2a396ee91ea7
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:57:49 -0400

    agregamos show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,8 @@ class TweetsController < ApplicationController
         @tweets = Tweet.all #si es un @, es una variable global. Vive más que śolo en este método, se puede acceder desde cualquier parte de la clase.
     end 
 
+    def show
+        @tweet = Tweet.find(params[:id])
+    end
+
 end
